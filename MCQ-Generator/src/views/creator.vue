240674e51558b036c488b7c88dd905aff58968d6
kshitijsharma <52919574+kshitijsharma@users.noreply.github.com>
2020-06-26 21:39:53 +0530

    Add files via upload

---
+++ MCQ-Generator/src/views/creator.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="app">
+    <el-card>
+        <h3>Category: <input type="text" v-model="data.category"/></h3>
+        <h3>Question: <input type="text" v-model="data.question"/></h3>
+        <h3>Answer: <input type="text" v-model="data.correct_answer"/></h3>
+        <h3>Options:</h3>
+        <h3>1.<input type="text" v-model="data.incorrect_answers[0]"/></h3>
+        <h3>2.<input type="text" v-model="data.incorrect_answers[1]"/></h3>
+        <h3>3.<input type="text" v-model="data.incorrect_answers[2]"/></h3>
+        <h3>4.<input type="text" v-model="data.incorrect_answers[3]"/></h3>
+        <el-button type="success" v-on:click="add()">Add Question</el-button>&emsp;
+        <el-button type="success" v-on:click="saveFile()">saveFile</el-button>&emsp;
+        <el-button type="success" v-on:click="navigate()">Go to your Quiz page</el-button>
+    </el-card>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: "App",
+  data(){
+    return{
+      data: {
+          category: "",
+          type: "multiple",
+          difficulty: "medium",
+          question:"",
+          correct_answer:"",
+          incorrect_answers:["","","",""]
+        },
+        arr:[]
+      };
+  },
+
+  methods: {
+      add() {
+          
+          this.arr.push({category: this.data.category, type: this.data.type, difficulty: this.data.difficulty, question: this.data.question,
+          correct_answer:this.data.correct_answer,incorrect_answers:this.data.incorrect_answers});
+          console.log(this.arr);
+      },
+      saveFile() {
+        const data = JSON.stringify(this.arr)
+        window.localStorage.setItem('arr', data);
+        const obj=JSON.parse(window.localStorage.getItem('arr'))
+        console.log(obj)
+      },
+      navigate(){
+          this.$router.push('/quiz')
+      }
+  }
+};
+</script>
---
+++ MCQ-Generator/src/views/quiz.vue
@@ -0,0 +1,184 @@
+<template>
+    <div>
+        <div v-for="(quiz, index) in quizez" v-show="index === questionindex" :key="index">
+        <h1>{{ quiz.category }}</h1>
+        <h2>{{ quiz.question }}</h2>
+        <el-card>
+        <ol>
+            <el-radio-group v-model="answers[index]">
+                <li v-for="answer in quiz.incorrect_answers" :key="answer">
+                    <el-radio name="answer" :label="answer"><b>{{answer}}</b></el-radio>
+                </li>
+            </el-radio-group>       
+        </ol>
+        </el-card>
+        </div><br><br>
+        <div v-if="questionindex < quizez.length">
+        <center>
+        <el-button type="danger" v-if="questionindex > 0" v-on:click="prev">
+        prev
+        </el-button>&emsp;
+        <el-button type="success" v-on:click="next">
+        next
+        </el-button>
+        </center>
+        </div>
+            <h1 v-if="questionindex == quizez.length">Your Total score is {{score}} / {{quizez.length}}</h1>
+        </div>
+</template>
+<script>
+var quiz_questions = [
+  {
+  "category": "Entertainment: Film",
+  "type": "multiple",
+  "difficulty": "easy",
+  "question": "Who directed E.T. the Extra-Terrestrial (1982)?",
+  "correct_answer": "Steven Spielberg",
+  "incorrect_answers": [
+  "Steven Spielberg",
+  "Stanley Kubrick",
+  "James Cameron",
+  "Tim Burton"
+  ]
+  },
+  {
+  "category": "Entertainment: Video Games",
+  "type": "multiple",
+  "difficulty": "medium",
+  "question": "What is the main character of Metal Gear Solid 2?",
+  "correct_answer": "Raiden",
+  "incorrect_answers": [
+  "Raiden",
+  "Solidus Snake",
+  "Big Boss",
+  "Venom Snake"
+  ]
+  },
+  {
+  "category": "Science & Nature",
+  "type": "multiple",
+  "difficulty": "easy",
+  "question": "What is the hottest planet in the Solar System?",
+  "correct_answer": "Venus",
+  "incorrect_answers": [
+  "Venus",
+  "Mars",
+  "Mercury",
+  "Jupiter"
+  ]
+  },
+  {
+  "category": "Entertainment: Books",
+  "type": "multiple",
+  "difficulty": "hard",
+  "question": "What is Ron Weasley's middle name?",
+  "correct_answer": "Bilius",
+  "incorrect_answers": [
+  "Bilius",
+  "Arthur",
+  "John",
+  "Dominic"
+  ]
+  },
+  {
+  "category": "Politics",
+  "type": "multiple",
+  "difficulty": "medium",
+  "question": "Before 2011, True Capitalist Radio was known by a different name. What was that name?",
+  "correct_answer": "True Conservative Radio",
+  "incorrect_answers": [
+  "True Conservative Radio",
+  "True Republican Radio",
+  "Texan Capitalist Radio",
+  "United Capitalists"
+  ]
+  },
+  {
+  "category": "Entertainment: Film",
+  "type": "multiple",
+  "difficulty": "medium",
+  "question": "This movie contains the quote, I love the smell of napalm in the morning!",
+  "correct_answer": "Apocalypse Now",
+  "incorrect_answers": [
+  "Apocalypse Now",
+  "Platoon",
+  "The Deer Hunter",
+  "Full Metal Jacket"
+  ]
+  },
+  {
+  "category": "History",
+  "type": "multiple",
+  "difficulty": "medium",
+  "question": "The Herero genocide was perpetrated in Africa by which of the following colonial nations?",
+  "correct_answer": "Germany",
+  "incorrect_answers": [
+  "Germany",
+  "Britain",
+  "Belgium",
+  "France"
+  ]
+  },
+  {
+  "category": "Entertainment: Books",
+  "type": "multiple",
+  "difficulty": "easy",
+  "question": "Under what pseudonym did Stephen King publish five novels between 1977 and 1984?",
+  "correct_answer": "Richard Bachman",
+  "incorrect_answers": [
+  "Richard Bachman",
+  "J. D. Robb",
+  "Mark Twain",
+  "Lewis Carroll"
+  ]
+  },
+  {
+  "category": "History",
+  "type": "multiple",
+  "difficulty": "medium",
+  "question": "In what prison was Adolf Hitler held in 1924?",
+  "correct_answer": "Landsberg Prison",
+  "incorrect_answers": [
+  "Landsberg Prison",
+  "Spandau Prison",
+  "Ebrach Abbey",
+  "Hohenasperg"
+  ]
+  }
+]
+export default {
+
+  name: 'app',
+  
+  data : function (){
+  return{
+    questionindex:0,
+    quizez:quiz_questions,
+    answers:Array(quiz_questions.length).fill(''),
+  }
+  },
+  methods: {
+        next: function() {
+        this.questionindex++;
+        },
+        prev: function() {
+        this.questionindex--;
+        }
+    },
+computed:{
+        score: function() {
+        var total = 0;
+        for (var i =0; i <this.answers.length; i++) {
+        if(this.answers[i]==this.quizez[i].correct_answer){total +=1;}
+        }
+        return total;
+        }
+    },
+    mounted(){
+        const obj=JSON.parse(window.localStorage.getItem('arr'))
+        for (var i =0; i <obj.length; i++) {
+            this.quizez.push(obj[i])
+        }
+    }
+}
+</script>
